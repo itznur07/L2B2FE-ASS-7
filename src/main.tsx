537b9a3cba5382da1a57751e4d56d8fb30963849
Md Nuruddin <nuruddinalways99@gmail.com>
2024-05-14 07:34:11 +0600

    create rtk store and provide whole application using provider. as well as create apiEndpoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { store } from "./redux/app/store";
 import { router } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div data-theme='light'>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </div>
   </React.StrictMode>
 );
