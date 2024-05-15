68fc310c7c29e9669285171a422c2e5f499446a8
Md Nuruddin <nuruddinalways99@gmail.com>
2024-05-15 19:50:46 +0600

    relief good view details done

--- src/components/Home/RelifGoods.tsx
+++ src/components/Home/RelifGoods.tsx
@@ -29,7 +29,29 @@ const RelifGoods = () => {
               </h2>
               <p>{item.description}</p>
               <div className='card-actions justify-end'>
-                <div className='btn'>View Details</div>
+                {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                <button
+                  className='btn'
+                  onClick={() =>
+                    document.getElementById("my_modal_3").showModal()
+                  }
+                >
+                  View Details
+                </button>
+                <dialog id='my_modal_3' className='modal'>
+                  <div className='modal-box'>
+                    <form method='dialog'>
+                      {/* if there is a button in form, it will close the modal */}
+                      <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
+                        ✕
+                      </button>
+                    </form>
+                    <h3 className='font-bold text-lg'>{item.title}</h3>
+                    <img src={item.image} alt='image' />
+                    <div className='badge badge-secondary'>{item.category}</div>
+                    <p className='py-4'>{item?.description}</p>
+                  </div>
+                </dialog>
               </div>
             </div>
           </div>
