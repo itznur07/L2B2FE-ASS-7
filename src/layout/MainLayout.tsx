bee255f1066cd5ddf7791371ef45d3943095fdaf
Md Nuruddin <nuruddinalways99@gmail.com>
2024-05-24 18:49:56 +0600

    route issue fixed

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/shared/Footer/Footer";
 import Navbar from "@/components/shared/Navbar/Navbar";
 import React, { ReactNode } from "react";
+import { Outlet } from "react-router-dom";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -10,7 +11,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className='main-layout'>
       <Navbar />
-      {children}
+      <Outlet />
       <Footer />
     </div>
   );
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <About />
-    </div>
+    </>
   );
 };
 
